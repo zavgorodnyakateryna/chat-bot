0ba257ee2f623942418476557a4b1a431efd55c7
zavgorodnyakaterina <katrin.zavgorodnaya@gmail.com>
2023-05-11 23:59:17 +0300

    create directory if it does not exist

--- logger.py
+++ logger.py
@@ -1,9 +1,16 @@
 import codecs
 from datetime import datetime
+import os
+
+dir = "./logs"
+
+isDirExist = os.path.exists(dir)
+if not isDirExist:
+    os.makedirs(dir)
 
 today = datetime.now()
 today_date = today.strftime("%d-%m-%Y-%H-%M")
-file_name = "./logs/dialog-" + today_date + ".txt"
+file_name = dir + "/dialog-" + today_date + ".txt"
 f = codecs.open(file_name, "w+", "utf-16") #w+ open to write (create file if it does not exist)
 def log(message):
     f.write(message + "\n")
